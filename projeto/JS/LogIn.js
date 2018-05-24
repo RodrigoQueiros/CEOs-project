ef4cc0791cfe0975ac5deabc63746a6f9a3c5197
RodrigoQueiros <rodrigodlq@hotmail.com>
2018-05-24 14:35:00 +0100

    style

--- projeto/JS/LogIn.js
+++ projeto/JS/LogIn.js
@@ -194,15 +194,13 @@ function loginChances(){
            else if(verifyType._type == "standard"){
             userType = "standard"
             changeLogin.innerHTML = `<li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              ` + verifyType._username + `
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              ` + verifyType._username+ `
             </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Perfil</a>
+              <a class="dropdown-item" href="#">Logout</a>           
+              </div>
           </li>`
 
 
